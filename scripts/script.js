d089dfcbbb3e198d82fbc7f35f94db13ad616c97
Vetiver <dmitriy_serga@mail.ru>
2022-04-25 22:56:50 +0300

    fixing-regular-cards

--- scripts/script.js
+++ scripts/script.js
@@ -1,122 +1,113 @@
-const infoDone = [
-  {
-    imageLink: './images/cards/laptop.png',
-    title: 'Инфогруппа',
-    text: 'Создание ориентировок, заказ карт, связь через мини АТС, обеспечение поиска',
-    level: 'Новичок'
-  },
-  {
-    imageLink: './images/cards/orange-jacket.png',
-    title: 'Новичковая',
-    text: 'Короткое описание курса. людей в лесу и в городе. Все поисковые мероприятия организуются силами добровольцев «ЛизаАлер…',
-    level: 'Новичок'
-  }
-];//Массив объектов для пройденных карточек
-
-const infoContinue = [
+const info = [
   {
     imageLink: './images/cards/german-shepherd.png',
     title: 'Кинологическое направление',
-    text: ' Поисково-спасательная работа, следовая работа, а так же поиск тел погибших с помощью собак',
-    level: 'Бывалый'
-  }
-];//Массив объектов для карточек с ,,продолжить,,
-
-const infoEnroll = [
+    text: 'Поисково-спасательная работа, следовая работа, а так же поиск тел погибших с помощью собак',
+    level: 'Бывалый',
+    status: 'Продолжить'
+  },
   {
     imageLink: './images/cards/radio-set.png',
     title: 'Оперативные дежурные',
     text: 'Оперативное реагирование, контроль поступающих заявок и звонков, распределение задач, помощь в решении вопросов, удалённое ...',
-    level: 'Профессионал'
+    level: 'Профессионал',
+    status: 'Записаться'
   },
-
   {
     imageLink: './images/cards/quadrocopter.png',
     title: 'Беспилотные летательные аппараты',
     text: 'Применение БПЛА в поиске людей, а так же передача полученной с помощью техники информации спасательным службам',
-    level: 'Бывалый'
+    level: 'Бывалый',
+    status: 'Записаться'
   },
-
   {
     imageLink: './images/cards/first-aid-kid.png',
     title: 'Первая помощь',
     text: 'Основы оказания первой помощи на поиске, юридические аспекты, базовые алгоритмы, разбор ошибок при оказания помощи на поиске',
-    level: 'Бывалый'
+    level: 'Бывалый',
+    status: 'Записаться'
+  },
+  {
+    imageLink: './images/cards/laptop.png',
+    title: 'Инфогруппа',
+    text: 'Создание ориентировок, заказ карт, связь через мини АТС, обеспечение поиска',
+    level: 'Новичок',
+    status: 'Пройден'
   },
   {
     imageLink: './images/cards/woman-in-a-striped-blouse.png',
     title: 'Операторы 8-800',
     text: 'Приём заявок на поиск людей с последующей передачей информации инфоргам',
-    level: 'Новичок'
+    level: 'Новичок',
+    status: 'Записаться'
   },
   {
     imageLink: './images/cards/woman-in-a-blazer.png',
     title: 'Группа коротких прозвонов',
     text: 'Прозвон больниц, ОВД, различных ведомств, иногда свидетелей и возможных свидетелей',
-    level: 'Новичок'
+    level: 'Новичок',
+    status: 'Записаться'
+  },
+  {
+    imageLink: './images/cards/orange-jacket.png',
+    title: 'Новичковая',
+    text: 'Короткое описание курса. людей в лесу и в городе. Все поисковые мероприятия организуются силами добровольцев «ЛизаАлер…',
+    level: 'Новичок',
+    status: 'Пройден'
   }
-];//Массив объектов для карточек с ,,записаться,,
+];
 
-const enrollTemplate = document.querySelector('#card-enroll').content;
-const continueTemplate = document.querySelector('#card-continue').content;
-const doneTemplate = document.querySelector('#card-done').content;
+
+
+
+const infoCardTemplate = document.querySelector('#tamplate-card').content;
 const cardContainer = document.querySelector('.cards__container');
 
-infoContinue.forEach((element) => {
-  continueAddCard(element.imageLink, element.title, element.text, element.level)
+info.forEach((element) => {
+  cardAdd(element.imageLink, element.title, element.text, element.level, element.status);
 });//Добавляет на страницу карточки с кнопкой ,,продолжить,, используя метод forEach
 
-infoEnroll.forEach((el) => {
-  enrollAddCard(el.imageLink, el.title, el.text, el.level)
-});//Добавляет на страницу карточки с кнопкой ,,записаться,, используя метод forEach
-
-infoDone.forEach((elements) => {
-  doneAddCard(elements.imageLink, elements.title, elements.text, elements.level);
-});//Добавляет на страницу карточки с кнопкой ,,пройдено,, используя метод forEach
-
-function continueAddCard(elementLink, elementTitle, elementText, elementLevel) {
-  const continueElement = continueTemplate.querySelector('#card-continueContent').cloneNode(true);
-  continueElement.querySelector('#image-continue').src = elementLink;
-  continueElement.querySelector('#image-continue').alt = elementTitle;
-  continueElement.querySelector('#title-continue').textContent = elementTitle;
-  continueElement.querySelector('#text-continue').textContent = elementText;
-  continueElement.querySelector('#medium').textContent = elementLevel;
-  cardContainer.append(continueElement);
+function cardAdd(elementLink, elementTitle, elementText, elementLevel, elementStatus) {
+  const element = infoCardTemplate.querySelector('.cards__item').cloneNode(true);
+  element.querySelector('#image-card').src = elementLink;
+  element.querySelector('#image-card').alt = elementTitle;
+  element.querySelector('.cards__title').textContent = elementTitle;
+  element.querySelector('.cards__description').textContent = elementText;
+  element.querySelector('.cards__user-level').textContent = elementLevel;
+  element.querySelector('.cards__button').textContent = elementStatus;
+  switch(elementStatus) {
+    case 'Пройден': element.querySelector('.cards__button').classList.add('cards__button_disabled');
+    break;
+    case 'Продолжить': element.querySelector('.cards__button').classList.add('cards__button_active');
+    break;
+  }
+  switch(elementLevel) {
+    case 'Новичок': element.querySelector('.cards__user-level').classList.add('cards__user-level_beginner');
+    break;
+    case 'Бывалый': element.querySelector('.cards__user-level').classList.add('cards__user-level_middle');
+    break;
+    case 'Профессионал': element.querySelector('.cards__user-level').classList.add('cards__user-level_advansed');
+    break;
+  }
+  cardContainer.append(element);
 };//Данная функция добавляет карточки с кнопкой ,,продолжить,,
 
-function enrollAddCard (elementLink, elementTitle, elementText, elementLevel) {
-  const enrollElement = enrollTemplate.querySelector('#card-enrollContent').cloneNode(true);
-  enrollElement.querySelector('#image-enroll').src = elementLink;
-  enrollElement.querySelector('#image-enroll').alt = elementTitle;
-  enrollElement.querySelector('#title-enroll').textContent = elementTitle;
-  enrollElement.querySelector('#text-enroll').textContent = elementText;
-  enrollElement.querySelector('#beginner').textContent = elementLevel;
-  cardContainer.append(enrollElement);
-};//Данная функция добавляет карточки с кнопкой ,,записаться,,
-
-function doneAddCard (elementLink, elementTitle, elementText, elementLevel) {
-  const doneElement = doneTemplate.querySelector('#card-doneContent').cloneNode(true);
-  doneElement.querySelector('#image-done').src = elementLink;
-  doneElement.querySelector('#image-done').alt = elementTitle;
-  doneElement.querySelector('#title-done').textContent = elementTitle;
-  doneElement.querySelector('#text-done').textContent = elementText;
-  doneElement.querySelector('#advanced').textContent = elementLevel;
-  cardContainer.append(doneElement);
-};//Данная функция добавляет карточки с кнопкой ,,пройдено,,
 
 /*-------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*-------------------------------------------------------добавление дефолтных карточек на страницу-------------------------------------------------------------------*/
 
-const filterCheckboxButtons = document.querySelectorAll('.filters__box_level');
-const filterCard = document.querySelectorAll('.cards__item');
 
-filterCheckboxButtons.forEach((target) => {
-  target.addEventListener('change', (evt) => {
-    
-  })
-})
   
   
 
 
 
+
+/**
+ * 
+ * @param {*} elementLink 
+ * @param {*} elementTitle 
+ * @param {*} elementText 
+ * @param {*} elementLevel 
+ * @param {*} elementButton 
+ */
